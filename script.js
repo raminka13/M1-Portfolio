6d6a9d1c173e8226c2c4526fa7bd2c53de8a6a2d
Raminka <raminka13@hotmail.com>
2022-06-13 18:53:32 -0500

    Add functions in scrips.js

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ const overlayMenu = document.querySelector(".navlinks");
 const closeB = document.querySelector(".mobile-menu-close");
 
 mobMenu.addEventListener("click", () => {
-  mobMenu.classList.toggle("active");
-  overlayMenu.classList.toggle("active");
-}) 
+    mobMenu.classList.toggle("active");
+    overlayMenu.classList.toggle("active");
+})
+
+closeB.addEventListener("click", () => {
+    mobMenu.classList.toggle("active");
+    overlayMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () => {
+        mobMenu.classList.remove("active");
+        overlayMenu.classList.remove("active");
+    }))
+
+document.querySelectorAll(".close").forEach(n => n.
+    addEventListener("click", () => {
+        mobMenu.classList.remove("active");
+        overlayMenu.classList.remove("active");
+    }))
