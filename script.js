82f47848847048c8807133c083b2aea006908856
Raminka <raminka13@hotmail.com>
2022-06-14 13:45:15 -0500

    Add array of projects to script.js

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+const projects = [
+  {
+    pName: 'Project One',
+    tags: ['HTML/CSS','Ruby On Rails','Javascript'],
+    pImg: '../Images/project1.jpg',
+    pText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consequat tincidunt mattis. Sed ornare sapien lacus, nec placerat leo tempus sed. Curabitur ac augue finibus augue gravida rhoncus ut efficitur tortor. Etiam ornare quam at ex fermentum vulputate. Ut semper, ex eu congue ullamcorper, ante lectus gravida augue, at mattis ipsum turpis ut neque. Ut volutpat lorem in sapien ultricies placerat non eu sapien. Praesent et tortor ut sapien porttitor tempus. Etiam sagittis fermentum nibh sagittis lacinia. Aenean id enim eu justo viverra blandit id vitae ex. Morbi blandit auctor quam, non aliquam ipsum. Maecenas eu sodales nisi. Nam eros est, pharetra nec eros quis, convallis blandit turpis. Praesent et tortor ut sapien porttitor tempus. Etiam sagittis fermentum nibh sagittis lacinia. Aenean id enim eu justo viverra blandit id vitae ex. Morbi blandit auctor quam, non aliquam ipsum. Maecenas eu sodales nisi. Nam eros est, pharetra nec eros quis, convallis blandit turpis.',
+    btnLive: 'https://raminka13.github.io/M1-Portfolio/',
+    btnSrc: 'https://github.com/raminka13/M1-Portfolio'
+  },
+  {
+    pName: 'Project Two',
+    tags: ['HTML/CSS','Ruby On Rails','Javascript'],
+    pImg: '../Images/project3.jpg',
+    pText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consequat tincidunt mattis. Sed ornare sapien lacus, nec placerat leo tempus sed. Curabitur ac augue finibus augue gravida rhoncus ut efficitur tortor. Etiam ornare quam at ex fermentum vulputate. Ut semper, ex eu congue ullamcorper, ante lectus gravida augue, at mattis ipsum turpis ut neque. Ut volutpat lorem in sapien ultricies placerat non eu sapien. Praesent et tortor ut sapien porttitor tempus. Etiam sagittis fermentum nibh sagittis lacinia. Aenean id enim eu justo viverra blandit id vitae ex. Morbi blandit auctor quam, non aliquam ipsum. Maecenas eu sodales nisi. Nam eros est, pharetra nec eros quis, convallis blandit turpis. Praesent et tortor ut sapien porttitor tempus. Etiam sagittis fermentum nibh sagittis lacinia. Aenean id enim eu justo viverra blandit id vitae ex. Morbi blandit auctor quam, non aliquam ipsum. Maecenas eu sodales nisi. Nam eros est, pharetra nec eros quis, convallis blandit turpis.',
+    btnLive: 'https://raminka13.github.io/M1-Portfolio/',
+    btnSrc: 'https://github.com/raminka13/M1-Portfolio'
+  },
+  {
+    pName: 'Project Three',
+    tags: ['HTML/CSS','Ruby On Rails','Javascript'],
+    pImg: '../Images/project3.jpg',
+    pText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consequat tincidunt mattis. Sed ornare sapien lacus, nec placerat leo tempus sed. Curabitur ac augue finibus augue gravida rhoncus ut efficitur tortor. Etiam ornare quam at ex fermentum vulputate. Ut semper, ex eu congue ullamcorper, ante lectus gravida augue, at mattis ipsum turpis ut neque. Ut volutpat lorem in sapien ultricies placerat non eu sapien. Praesent et tortor ut sapien porttitor tempus. Etiam sagittis fermentum nibh sagittis lacinia. Aenean id enim eu justo viverra blandit id vitae ex. Morbi blandit auctor quam, non aliquam ipsum. Maecenas eu sodales nisi. Nam eros est, pharetra nec eros quis, convallis blandit turpis. Praesent et tortor ut sapien porttitor tempus. Etiam sagittis fermentum nibh sagittis lacinia. Aenean id enim eu justo viverra blandit id vitae ex. Morbi blandit auctor quam, non aliquam ipsum. Maecenas eu sodales nisi. Nam eros est, pharetra nec eros quis, convallis blandit turpis.',
+    btnLive: 'https://raminka13.github.io/M1-Portfolio/',
+    btnSrc: 'https://github.com/raminka13/M1-Portfolio'
+  },
+  {
+    pName: 'Project Four',
+    tags: ['HTML/CSS','Ruby On Rails','Javascript'],
+    pImg: '../Images/project4.jpg',
+    pText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consequat tincidunt mattis. Sed ornare sapien lacus, nec placerat leo tempus sed. Curabitur ac augue finibus augue gravida rhoncus ut efficitur tortor. Etiam ornare quam at ex fermentum vulputate. Ut semper, ex eu congue ullamcorper, ante lectus gravida augue, at mattis ipsum turpis ut neque. Ut volutpat lorem in sapien ultricies placerat non eu sapien. Praesent et tortor ut sapien porttitor tempus. Etiam sagittis fermentum nibh sagittis lacinia. Aenean id enim eu justo viverra blandit id vitae ex. Morbi blandit auctor quam, non aliquam ipsum. Maecenas eu sodales nisi. Nam eros est, pharetra nec eros quis, convallis blandit turpis. Praesent et tortor ut sapien porttitor tempus. Etiam sagittis fermentum nibh sagittis lacinia. Aenean id enim eu justo viverra blandit id vitae ex. Morbi blandit auctor quam, non aliquam ipsum. Maecenas eu sodales nisi. Nam eros est, pharetra nec eros quis, convallis blandit turpis.',
+    btnLive: 'https://raminka13.github.io/M1-Portfolio/',
+    btnSrc: 'https://github.com/raminka13/M1-Portfolio'
+  },
+  {
+    pName: 'Project Five',
+    tags: ['HTML/CSS','Ruby On Rails','Javascript'],
+    pImg: '../Images/project5.jpg',
+    pText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consequat tincidunt mattis. Sed ornare sapien lacus, nec placerat leo tempus sed. Curabitur ac augue finibus augue gravida rhoncus ut efficitur tortor. Etiam ornare quam at ex fermentum vulputate. Ut semper, ex eu congue ullamcorper, ante lectus gravida augue, at mattis ipsum turpis ut neque. Ut volutpat lorem in sapien ultricies placerat non eu sapien. Praesent et tortor ut sapien porttitor tempus. Etiam sagittis fermentum nibh sagittis lacinia. Aenean id enim eu justo viverra blandit id vitae ex. Morbi blandit auctor quam, non aliquam ipsum. Maecenas eu sodales nisi. Nam eros est, pharetra nec eros quis, convallis blandit turpis. Praesent et tortor ut sapien porttitor tempus. Etiam sagittis fermentum nibh sagittis lacinia. Aenean id enim eu justo viverra blandit id vitae ex. Morbi blandit auctor quam, non aliquam ipsum. Maecenas eu sodales nisi. Nam eros est, pharetra nec eros quis, convallis blandit turpis.',
+    btnLive: 'https://raminka13.github.io/M1-Portfolio/',
+    btnSrc: 'https://github.com/raminka13/M1-Portfolio'
+  },
+  {
+    pName: 'Project Six',
+    tags: ['HTML/CSS','Ruby On Rails','Javascript'],
+    pImg: '../Images/project6.jpg',
+    pText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consequat tincidunt mattis. Sed ornare sapien lacus, nec placerat leo tempus sed. Curabitur ac augue finibus augue gravida rhoncus ut efficitur tortor. Etiam ornare quam at ex fermentum vulputate. Ut semper, ex eu congue ullamcorper, ante lectus gravida augue, at mattis ipsum turpis ut neque. Ut volutpat lorem in sapien ultricies placerat non eu sapien. Praesent et tortor ut sapien porttitor tempus. Etiam sagittis fermentum nibh sagittis lacinia. Aenean id enim eu justo viverra blandit id vitae ex. Morbi blandit auctor quam, non aliquam ipsum. Maecenas eu sodales nisi. Nam eros est, pharetra nec eros quis, convallis blandit turpis. Praesent et tortor ut sapien porttitor tempus. Etiam sagittis fermentum nibh sagittis lacinia. Aenean id enim eu justo viverra blandit id vitae ex. Morbi blandit auctor quam, non aliquam ipsum. Maecenas eu sodales nisi. Nam eros est, pharetra nec eros quis, convallis blandit turpis.',
+    btnLive: 'https://raminka13.github.io/M1-Portfolio/',
+    btnSrc: 'https://github.com/raminka13/M1-Portfolio'
+  },
+];
+
+
+
 const mobMenu = document.querySelector('.mobile-menu');
 const overlayMenu = document.querySelector('.navlinks');
 const closeB = document.querySelector('.mobile-menu-close');
