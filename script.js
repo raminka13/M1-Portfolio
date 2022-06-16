2171f43073c745a15fd1df6d77904408ebc4cd70
karlavdelgadof <karlavdelgadof@gmail.com>
2022-06-16 16:51:54 -0500

    Add subBtn onclick

--- script.js
+++ script.js
@@ -247,3 +247,13 @@ const subBtn = document.getElementById('submitbtn');
 const errMsg = document.getElementById('error_msg');
 const mailForm = document.getElementById('email-address');
 
+subBtn.onclick = (event) => {
+  const mailValue = document.getElementById('email-address').value;
+  const lowerMail = mailValue.toLowerCase();
+  
+  if (mailValue !== lowerMail) {
+  errMsg.textContent = 'Format is not correct';
+  mailForm.classList.toggle('email_error');
+  event.preventDefault();
+  }
+  }; 
