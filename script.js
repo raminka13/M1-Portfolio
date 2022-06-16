7f29da09eee30d0eb9e2109a0f53013018ef517e
Raminka <raminka13@hotmail.com>
2022-06-16 13:45:11 -0500

    Script final functions

--- script.js
+++ script.js
@@ -55,77 +55,7 @@ const projectsArr = [
   },
 ];
 
-const overlayModal = document.querySelector('.modal-overlay');
-
-function createModal(_project) {
-  const modCont = document.createElement('div');
-  const modName = document.createElement('h2');
-  const lang = document.createElement('div');
-  const langList = document.createElement('ul');
-  const modImg = document.createElement('img');
-  const modText = document.createElement('div');
-  const modP = document.createElement('p');
-  const liveBtn = document.createElement('a');
-  const srcBtn = document.createElement('a');
-  const modTitle = document.createTextNode(_project.pName);
-  const modParr = document.createTextNode(_project.pText);
-
-  const btnClose = document.createElement('div');
-  modCont.appendChild(btnClose);
-  btnClose.className = 'modal-menu-close';
-  btnClose.addEventListener('click', closeModal);
-
-
-    for(let i = 0; i < 3 ; i += 1){
-      const bar = document.createElement('span');
-      bar.className = 'bar';
-      btnClose.appendChild(bar);
-    };
-  
-  modCont.className = 'proj-wrp-mod';
-  modCont.appendChild(modName);
-
-  modName.className = 'projtxt-mod';
-  modName.appendChild(modTitle);
-
-  modCont.appendChild(lang);
-  lang.appendChild(langList);
-
-  _project.tags.forEach((_element, index) => {
-    const tag = document.createElement('li');
-    const tagBtn = document.createElement('button');
-
-    tagBtn.className = 'smallbtn-mod';
-    tagBtn.textContent = _project.tags[index];
-    langList.appendChild(tag);
-    tag.appendChild(tagBtn);
-  });
-  lang.className = 'projlang-mod';
-
-  modImg.src = _project.pImg;
-  modImg.alt = _project.pName;
-  modImg.className = 'projectimg-mod';
-
-  modText.appendChild(modP);
-  modP.appendChild(modParr);
-  modText.className = 'mod-wrpr';
-  modP.className = 'mtext-mod';
-
-  liveBtn.textContent = 'See live';
-  liveBtn.href = _project.btnLive
-  liveBtn.className = 'projbtn-mod';
-
-  srcBtn.textContent = 'See source';
-  srcBtn.href = _project.btnSrc
-  srcBtn.className = 'projbtn-mod-src';
-
-  overlayModal.appendChild(modCont);
-  modCont.appendChild(modImg);
-  modCont.appendChild(modText);
-  modText.appendChild(liveBtn);
-  modText.appendChild(srcBtn);
-};
-
+/////// projects ///////
 const projContainer = document.getElementById('projects');
 
 function createProject(project) {
@@ -141,7 +71,6 @@ function createProject(project) {
   const nameProj = document.createTextNode(project.pName);
 
   projDiv.className = 'project';
-  projDiv.dataset.proj = project.pId;
 
   projDeco.className = 'deco' + (project.pId + 2);
   projDeco1.className = 'deco' + (project.pId + 3);
@@ -149,9 +78,12 @@ function createProject(project) {
   img.src = project.pImg;
   img.alt = project.pName;
   img.className = 'projectimg';
-  img.addEventListener('click', showModal);
 
-  pWrp.className = 'projectwrp';
+  if (project.pId == 1 || project.pId == 6) {
+    pWrp.className = 'projectwrp mproj';
+  } else {
+    pWrp.className = 'projectwrp';
+  }
 
   name.appendChild(nameProj);
   name.className = 'projtxt';
@@ -173,7 +105,7 @@ function createProject(project) {
 
   pBtn.textContent = 'See this project';
   pBtn.className = 'projbtn';
-  pBtn.addEventListener('click', showModal);
+  pBtn.setAttribute('id', project.pId);
 
   projContainer.appendChild(projDiv);
   projContainer.appendChild(projDeco);
@@ -182,23 +114,114 @@ function createProject(project) {
   projDiv.appendChild(pWrp);
   pWrp.appendChild(name);
   pWrp.appendChild(pBtn);
-};
+}
 projectsArr.forEach(createProject);
 
-function showModal(e) {
-  createModal(projectsArr[5]);
-  overlayModal.classList.toggle('active');
-};
+////// modal //////////
+const overlayModal = document.querySelector('.modal-overlay');
 
-function closeModal(e) {
+function createModal(id) {
+  projectIdModal = id;
+  const project = projectsArr.find((project) => Number(project.pId) === Number(id));
+  const modCont = document.createElement('div');
+  const modName = document.createElement('h2');
+  const lang = document.createElement('div');
+  const langList = document.createElement('ul');
+  const modImg = document.createElement('img');
+  const modText = document.createElement('div');
+  const modP = document.createElement('p');
+  const liveBtn = document.createElement('a');
+  const srcBtn = document.createElement('a');
+  const modTitle = document.createTextNode(project.pName);
+  const modParr = document.createTextNode(project.pText);
+
+  const btnClose = document.createElement('div');
+  modCont.appendChild(btnClose);
+  btnClose.className = 'modal-menu-close';
+  btnClose.addEventListener('click', closeModal);
+
+    for(let i = 0; i < 3 ; i += 1){
+      const bar = document.createElement('span');
+      bar.className = 'bar';
+      btnClose.appendChild(bar);
+    };
+  
+  modCont.className = 'proj-wrp-mod';
+  modCont.appendChild(modName);
+
+  modName.className = 'projtxt-mod';
+  modName.appendChild(modTitle);
+
+  modCont.appendChild(lang);
+  lang.appendChild(langList);
+
+  project.tags.forEach((_element, index) => {
+    const tag = document.createElement('li');
+    const tagBtn = document.createElement('button');
+
+    tagBtn.className = 'smallbtn-mod';
+    tagBtn.textContent = project.tags[index];
+    langList.appendChild(tag);
+    tag.appendChild(tagBtn);
+  });
+  lang.className = 'projlang-mod';
+
+  modImg.src = project.pImg;
+  modImg.alt = project.pName;
+  modImg.className = 'projectimg-mod';
+
+  modText.appendChild(modP);
+  modP.appendChild(modParr);
+  modText.className = 'mod-wrpr';
+  modP.className = 'mtext-mod';
+
+  liveBtn.textContent = 'See live';
+  liveBtn.href = project.btnLive;
+  liveBtn.className = 'projbtn-mod';
+
+  srcBtn.textContent = 'See source';
+  srcBtn.href = project.btnSrc;
+  srcBtn.className = 'projbtn-mod-src';
+
+  overlayModal.appendChild(modCont);
+  modCont.appendChild(modImg);
+  modCont.appendChild(modText);
+  modText.appendChild(liveBtn);
+  modText.appendChild(srcBtn);
+}
+
+function cleanModal() {
+  const div = overlayModal;
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
+  }
+}
+
+function openModal() {
+  overlayModal.classList.toggle('active');
+  window.onclick = function(event) {
+    if (event.target === overlayModal) {
+      overlayModal.classList.remove('active');
+    }
+  }
+}
+
+function closeModal() {
   overlayModal.classList.remove('active');
-};
+}
+
+projContainer.addEventListener('click', (e) => {
+  const elementId = e.target.id;
+  if (e.target.classList.contains('projbtn')) {
+    cleanModal(overlayModal.id);
+    createModal(elementId);
+    openModal();
+  }
+})
 
 const mobMenu = document.querySelector('.mobile-menu');
 const overlayMenu = document.querySelector('.navlinks');
 const closeB = document.querySelector('.mobile-menu-close');
-const closeMod = document.querySelector('.modal-menu-close');
-
 
 mobMenu.addEventListener('click', () => {
   mobMenu.classList.toggle('active');
@@ -222,7 +245,3 @@ document.querySelectorAll('.mobile-menu-close').forEach((n) => n
     overlayMenu.classList.remove('active');
   }));
 
-// document.querySelectorAll('.modal-menu-close').forEach((n) => n
-//   .addEventListener('click', () => {
-//     overlayModal.classList.remove('active');
-//   }));
